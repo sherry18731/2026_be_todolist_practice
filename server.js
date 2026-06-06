ebcf6e550c8cf4504e236ac14b3ff9dc5de0d0ac
Sherry Wu <shw18731@gmail.com>
2026-06-06 17:56:59 +0800

    init

--- server.js
+++ server.js
@@ -81,8 +81,5 @@ const requestListener = (req, res) => {
   }
 };
 
-const PORT = 3000;
 const server = http.createServer(requestListener);
-server.listen(PORT, () => { 
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+server.listen(process.env.PORT || 3005);
